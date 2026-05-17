fda48f4220fb24dfa2d1d0acc0851b8cbc1d4c17
himesh <himeshbaraik59@gmail.com>
2026-05-17 13:39:34 +0530

    distinct numbers

---
+++ Searching & Sorting/distinct_numbers.cpp
@@ -0,0 +1,55 @@
+// Includes all the standard headers
+#include <bits/stdc++.h>
+using namespace std;
+
+// Aliases
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+// constants:
+constexpr ll INF = 2e18;
+constexpr ld EPS = 1e-9;
+constexpr ll MOD = 998244853; // 1e9 + 7;
+
+// common definitions:
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define endl '\n'
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+//helpers:
+void print_v(vector<ll>& v){
+    for(ll& x: v){
+        cout<<x<<" ";
+    }
+    cout<<endl;
+}
+
+
+// logic for one test case:
+void solve(){
+    int n;
+    vector<int> arr;
+    cin>>n;
+    for(int i = 0; i < n; i++){
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    set<int> distinct(arr.begin(), arr.end());
+    cout << distinct.size() << endl;
+}
+
+// main handles test cases
+int main(){
+    fast_io;
+
+    int t=1;
+    // cin>>t; // uncomment if multiple test cases
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
